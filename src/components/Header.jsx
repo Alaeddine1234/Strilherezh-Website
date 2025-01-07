a7d7be0515301e0b525d0cd8de41dc31ab083514
Alaeddine1234 <jemiliala08@gmail.com>
2025-01-07 17:35:37 +0100

    Modification

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,16 +21,29 @@ const Header = () => {
 
             {/* Center: Logo */}
             <div className="flex items-center justify-center">
-              <ShoppingBasket className="h-8 w-8 text-[#E0AA3E]" />
+              <div className="text-center">
+                <ShoppingBasket className="h-8 w-8 sm:h-10 sm:w-10 text-[#E0AA3E]" />
+                <h1 className="text-sm sm:text-base font-semibold text-[#E0AA3E]">
+                  STRILHEREZH
+                </h1>
+              </div>
             </div>
 
             {/* Right: Account and Cart */}
             <div className="flex items-center space-x-4">
-              <a href="#" className="text-[#505050] hover:text-[#AAAAAA]">
-                <User className="h-5 w-5" />
+              <a
+                href="#"
+                className="flex items-center space-x-2 text-[#505050] hover:text-[#AAAAAA]"
+              >
+                <User className="h-5 w-5 sm:h-6 sm:w-6" />
+                <span className="hidden lg:block text-sm">Compte</span>
               </a>
-              <a href="#" className="text-[#505050] hover:text-[#AAAAAA]">
-                <ShoppingBasket className="h-5 w-5" />
+              <a
+                href="#"
+                className="flex items-center space-x-2 text-[#505050] hover:text-[#AAAAAA]"
+              >
+                <ShoppingBasket className="h-5 w-5 sm:h-6 sm:w-6" />
+                <span className="hidden lg:block text-sm">Panier</span>
               </a>
             </div>
           </div>
@@ -40,12 +53,16 @@ const Header = () => {
       {/* Golden banner */}
       <div className="bg-[#E0AA3E]">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between h-12">
+          <div className="flex flex-wrap items-center justify-between h-12">
+            {/* Left: Banner logo */}
             <div className="flex items-center space-x-2">
               <ShoppingBasket className="h-5 w-5 text-white" />
-              <span className="text-white font-medium text-sm">STRILHEREZH</span>
+              <span className="text-white font-medium text-sm sm:text-base">
+                STRILHEREZH
+              </span>
             </div>
-            <div className="flex space-x-6">
+            {/* Right: Navigation links */}
+            <div className="flex space-x-4 sm:space-x-6">
               <a href="#" className="text-white hover:text-[#E3E3E3] text-sm font-medium">
                 Tous nos produits
               </a>
@@ -58,12 +75,22 @@ const Header = () => {
       </div>
 
       {/* Mobile menu */}
-      <div className={`${isMenuOpen ? 'block' : 'hidden'} sm:hidden bg-white border-b`}>
+      <div
+        className={`${
+          isMenuOpen ? 'block' : 'hidden'
+        } sm:hidden bg-white border-b`}
+      >
         <div className="px-4 py-2 space-y-1">
-          <a href="#" className="block py-2 text-sm text-[#505050] hover:text-[#AAAAAA]">
+          <a
+            href="#"
+            className="block py-2 text-sm text-[#505050] hover:text-[#AAAAAA]"
+          >
             Tous nos produits
           </a>
-          <a href="#" className="block py-2 text-sm text-[#505050] hover:text-[#AAAAAA]">
+          <a
+            href="#"
+            className="block py-2 text-sm text-[#505050] hover:text-[#AAAAAA]"
+          >
             Notre histoire
           </a>
         </div>
